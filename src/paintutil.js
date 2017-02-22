f4103b759875d551e35f64e675edc16d92fa5f44
Twinone <twinonedevs@gmail.com>
2017-02-22 12:36:44 +0100

    Remove dst

--- src/paintutil.js
+++ src/paintutil.js
@@ -38,17 +38,8 @@ function drawLine(ctx, x1, y1, x2, y2, offset1, offset2, c) {
   ctx.stroke()
 }
 
-
-
-function dst(x1, y1, x2, y2) {
-  var dx = x2 - x1,
-    dy = y2 - y1
-  return Math.sqrt(dx * dx + dy * dy)
-}
-
 module.exports = {
   drawCircle: drawCircle,
   drawText: drawText,
   drawLine: drawLine,
-  dst:dst,
 };
